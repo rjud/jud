0d4e4380993cc37858056285ab07b80ae543924f
J. Bedouet <j.bedouet@free.fr>
2014-04-20 20:23:18 +0200

    Add the class Version.

---
+++ Library/version.rb
@@ -0,0 +1,19 @@
+class Version < Array
+  
+  def initialize str
+    super(str.split('.').map { |v| v.to_i })
+  end
+  
+  def < v
+    (self <=> v) < 0
+  end
+  
+  def > v
+    (self <=> v) > 0
+  end
+  
+  def == v
+    (self <=> v) == 0
+  end
+  
+end
