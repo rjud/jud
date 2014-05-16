f0bc8c3deb7f8a22eee3d7cdf531211d13593b62
Antoine Joulia <joulia@ldprs701.onecert.fr>
2014-05-16 16:00:25 +0200

    minor change

--- Library/build_tool.rb
+++ Library/build_tool.rb
@@ -32,7 +32,7 @@ class BuildTool < Tool
     ret = {}
     # Eval the default options of the project
     @options.each do |id, opt|
-      if not opt.default.nil? and (opt.cond.nil? or options.key? opt.cond or ( ret.key? opt.cond and ret[opt.cond].value) ) then
+      if opt.default and (opt.cond.nil? or options.key? opt.cond or ( ret.key? opt.cond and ret[opt.cond].value) ) then
         ret[id] = ResolvedOption.new id.to_s, opt.type, true, (Project.project_eval opt.default)
       end
     end
