4f776267a660d077a93eecea9b91c13d38624513
J. Bedouet <j.bedouet@free.fr>
2015-09-09 23:39:53 +0200

    Make: add the method build_command_line

--- Tools/make.rb
+++ Tools/make.rb
@@ -7,19 +7,23 @@ class Make < BuildTool
   def initialize options = {}
     super()
   end
-  
-  def build build, options = {}
+
+  def build_command_line build, options
     cmd = '"' + path + '"'
     mono = (options.has_key? :mono) and options[:mono]
     cmd += ' -j3' if Platform.is_linux? and not mono
     cmd += " #{options[:target]}" if options.has_key? :target
-    Platform.execute cmd, wd: build
+    cmd
   end
   
-  def install build, options = {}
+  def build builddir, options = {}
+    Platform.execute (build_command_line builddir, options), wd: builddir
+  end
+  
+  def install builddir, options = {}
     fast = (options.has_key? :fast) and options[:fast]
     cmd = '"' + path + '" install' + ('/fast' if fast)
-    Platform.execute cmd, wd: build
+    Platform.execute cmd, wd: builddir
   end
   
 end
