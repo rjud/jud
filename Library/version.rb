a96d5092d63e088edd01bcec2d866d3acb765c9a
J. Bedouet <j.bedouet@free.fr>
2015-09-14 21:44:41 +0200

    major, minor release

--- Library/version.rb
+++ Library/version.rb
@@ -1,8 +1,11 @@
 module Jud
   class Version < Array
     
+    attr_reader :major, :minor, :release
+    
     def initialize str
       super(str.split('.').map { |v| v.to_i })
+      @major, @minor, @release = self[0], self[1], self[2]
     end
     
     def < v
