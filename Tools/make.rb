853852ab1728fc7ef28089ed1f4954bf699ff990
J. Bedouet <j.bedouet@free.fr>
2015-09-15 10:08:24 +0200

    Add options to make. Add configure to make to have a complete build tool.

--- Tools/make.rb
+++ Tools/make.rb
@@ -12,6 +12,8 @@ class Make < BuildTool
     cmd = '"' + path + '"'
     mono = (options.has_key? :mono) and options[:mono]
     cmd += ' -j3' if Platform.is_linux? and not mono
+    cmd += " -f #{options[:makefile]}" if options.has_key? :makefile
+    cmd += " #{options[:args]}" if options.has_key? :args
     cmd += " #{options[:target]}" if options.has_key? :target
     cmd
   end
@@ -20,7 +22,10 @@ class Make < BuildTool
     cmd = build_command_line (if rule.nil? then options else options.merge({ :target => rule }) end)
     Platform.execute cmd, options
   end
-  
+
+  def configure src, build, install, build_type, prj, options={}
+  end
+
   def build builddir, options = {}
     execute nil, options.merge({ :wd => builddir })
   end
