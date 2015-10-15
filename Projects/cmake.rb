3133e982d0ac6e6e5b12736e270bb4cbae766c92
J. Bedouet <j.bedouet@free.fr>
2015-10-15 19:14:22 +0200

    Solve bug in the build of the CMake project

--- Projects/cmake.rb
+++ Projects/cmake.rb
@@ -1,16 +1,14 @@
 class CMake < Project
   
   cxx
-  wget 'http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz', (Tarball.new '.gz')
+  wget 'http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz', (Tarball.new '.gz'), { :srcrename => 'cmake-3.2.2' }
   autotools
   
-  def configure_this src, build, install, build_type, options={}
-    bootstrap = File.join(src, 'bootstrap')
-    Platform.execute "#{bootstrap}", wd: build
+  configure do
+    run "#{src}/bootstrap --prefix=#{prefix}"
   end
   
-  def build_types; [:Release]; end
-
+  def build_types; [:Release]; end  
   def bin_dir; prefix.join('bin').to_s; end
   
 end
